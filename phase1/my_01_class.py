ead19b150027b0b95337216af597e3ce6931be5d
Monika2019 <52123265+Monika2019@users.noreply.github.com>
2019-07-28 15:21:47 +0800

    01

---
+++ phase1/my_01_class.py
@@ -0,0 +1,26 @@
+# -*- coding: UTF-8 -*-
+class Myclass:
+    name = '张三'
+    sex = '女'
+    __bank_account = '987654321'
+    __bank_password = 'hello123'
+
+
+    def work(self):
+        print('I\'m working...')
+        return
+
+
+    def study(self):
+        print('I\'m studying...')
+        return
+
+
+    def __drink(self):
+        print('I\'m drinking...')
+        return
+
+
+    def __smoking(self):
+        print('I\'m smoking...')
+        return
